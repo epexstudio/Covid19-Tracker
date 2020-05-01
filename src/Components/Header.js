8db9666ba55f6c20fb9631502b18661f047f7e6a
Anu S Kumar <64139878+epexstudio@users.noreply.github.com>
2020-05-01 16:43:55 +0530

    Update Header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,23 +1,51 @@
 import React , {Component} from 'react';
-import {Link} from 'react-router-dom';
-import {Navbar,Nav,Form,FormControl,Button} from 'react-bootstrap';
-
-class Header extends Component{
+import logo from "./covid.png";
+import {Card} from 'react-bootstrap';
+import Axios from 'axios';
+import './Home.css'
+class Home extends Component{
+    constructor(){
+        super();
+        this.state = {
+            data : []
+        }
+    }
+    componentDidMount(){
+        Axios.get("https://api.covid19api.com/world/total").then(response=>
+        {
+             this.setState({data:response.data});
+       });
+    }
     render(){
         return(
-            <Navbar bg="dark" variant="dark">
-                <Navbar.Brand href="/home"><i class="fas fa-shield-virus"style={{fontSize:'50px'}}></i></Navbar.Brand>
-                <Nav className="mr-auto">
-                <Link className="nav-link" to="/world">World</Link>
-                <Link className="nav-link" to="/india">India</Link>
-            </Nav>
-            <Form inline>
-                <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-                <Button variant="outline-light">Search</Button>
-            </Form>
-            </Navbar>
+            <div className="heading">
+                <img image-fluid src= {logo} />
+                <h3>Tracker</h3>
+                <Card className="badge badge-warning" style={{ width: '18rem',marginRight:'10px',color:'#000'}}>
+                            <Card.Body className="text-center">
+                                <Card.Title>TOTAL CONFIRMED</Card.Title>
+                                <h3 style={{letterSpacing:'1px'}}>{this.state.data.TotalConfirmed}
+                                    </h3>
+                                <Card.Text></Card.Text>
+                            </Card.Body>
+                        </Card>
+                        <Card className="badge badge-danger" style={{ width: '18rem',color:'#000'}}>
+                            <Card.Body className="text-center">
+                                <Card.Title>DEATHS</Card.Title>
+                                <h3 style={{letterSpacing:'1px'}}>{this.state.data.TotalDeaths}</h3>
+                                <Card.Text></Card.Text>
+                            </Card.Body>
+                        </Card>
+                        <Card className="badge badge-success" style={{ width: '18rem',marginLeft:'10px',color:'#000'}}>
+                            <Card.Body className="text-center">
+                                <Card.Title>TOTAL RECOVERED</Card.Title>
+                                <h3 style={{letterSpacing:'1px'}}>{this.state.data.TotalRecovered}</h3>
+                                <Card.Text></Card.Text>
+                            </Card.Body>
+                        </Card>
+            </div>
         )
+        
     }
 }
-
-export default Header;
+export default Home;
